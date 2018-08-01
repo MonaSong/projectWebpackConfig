ce8f9ab9add4470848d3857497ec02c222d03e60
qqsong <qqsong@niub.la>
2018-08-01 17:37:51 +0800

    优化element-ui的配置

--- src/js/index.js
+++ src/js/index.js
@@ -3,7 +3,6 @@ import App from './containers/App.vue';
 import router from './router';
 import '../assets/style/index.css';
 import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
 import store from './store/index';
 
 Vue.use(ElementUI);
